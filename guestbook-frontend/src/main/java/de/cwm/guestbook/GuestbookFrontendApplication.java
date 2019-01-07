486c02cfd0e5ead8346e0d8622818d8ff1077756
mm <mm@CWM33.gernsheim.compwaremedical.de>
2019-01-07 13:55:32 +0100

    create a new project for use as frontend

---
+++ guestbook-frontend/src/main/java/de/cwm/guestbook/GuestbookFrontendApplication.java
@@ -0,0 +1,14 @@
+package de.cwm.guestbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestbookFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestbookFrontendApplication.class, args);
+	}
+
+}
+
